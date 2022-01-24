a7eabc9f4cb7ff9dc881b42754670971816dfc1a
arielmell0 <arielmello@rede.ulbra.br>
2022-01-23 22:59:57 -0300

    feat: validador de CPF concluido
    gerador de cpf concluido, html e css ajustados

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import GeraCPF from './modules/GeraCPF';
 
 import './assets/css/style.css';
+import ValidaCPF from './modules/ValidaCPF';
 
+// Gerador de CPF
 (function() {
   const gera = new GeraCPF();
   const cpfGerado = document.querySelector('.cpf-gerado');
@@ -18,3 +20,31 @@ import './assets/css/style.css';
     }  
   })
 })();
+
+// Validador de CPF
+(function() {
+  const input = document.querySelector('#valida-cpf')
+  const btnValida = document.querySelector('#btn-valida-cpf')
+  const resultado = document.querySelector('#valida-resultado')
+
+  document.addEventListener('click', event => {
+    const valida = new ValidaCPF(input.value)
+    const target = event.target
+
+    if(target == btnValida){
+      if(valida.éSequência()){
+        return resultado.innerHTML = `CPF inválido, não é permitido sequências!`
+      }
+
+      if(valida.cpfLimpo.length !== 11) {
+        return resultado.innerHTML = 'Ops, você precisa seguir o formato XXX.XXX.XXX-XX'
+      }
+
+      if(valida.valida()){
+        return resultado.innerHTML = 'CPF é Válido!'
+      } else {
+        return resultado.innerHTML = 'CPF inválido!'
+      }
+    }
+  })
+})()
